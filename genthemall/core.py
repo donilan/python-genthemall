a21a80393f5f198caa775f0880fb955d9a44c0a2
Doni <donilan.qq@gmail.com>
2013-12-02 16:10:09 +0800

    GTLGenerator complete.

--- genthemall/core.py
+++ genthemall/core.py
@@ -1,9 +1,47 @@
-import logging, os, re, io
-from config import Config
+import logging, os, re, io, sys
+from config import Config, Sequence, Mapping
 from genthemall.version import get_version
 from mako.template import Template as MakoTemplate
 
 log = logging.getLogger('genthemall.core')
+
+default_max_map = {
+    'long': 1 << 64,
+    'int': 1 << 32,
+    'string': 256,
+    'text': 1024,
+    'date': None,
+}
+
+default_min_map = {
+    'long': -1 << 64,
+    'int': -1 << 32,
+    'string': 0,
+    'text': 0,
+    'date': None,
+}
+
+java_type_map = {
+        'long': 'java.lang.Long',
+        'int': 'java.lang.Integer',
+        'string': 'java.lang.String',
+        'text': 'java.lang.String',
+        'date': 'java.util.Date',
+}
+java_short_type_map = {
+        'long': 'long',
+        'int': 'int',
+        'string': 'String',
+        'text': 'String',
+        'date': 'java.util.Date',
+}
+database_type_map = {
+        'long': 'NUMBER',
+        'int': 'NUMBER',
+        'string': 'VARCHAR2',
+        'text': 'VARCHAR2',
+        'date': 'DATETIME',
+}
 
 class GTLTemplate:
     """Template object for store in application runtime."""
@@ -51,6 +89,13 @@ class GTLTemplateHolder():
         if self.user_templates:
             list_fn(self.user_templates, 'User templates:')
 
+    def find_template_by_name(self, name):
+        """Find sepecify template by name."""
+        for t in self.sys_templates + self.user_templates:
+            if t.config.name == name:
+                return t
+        return None
+
     def _init_templates(self):
         self._init_template_files()
         """Find template files and make it in to Template."""
@@ -91,21 +136,189 @@ class GTLTemplateHolder():
                 return GTLTemplate(config, content, path)
         return None
 
+### GTLTemplateHolder
 
 class GTLGenerator():
-
-    def __init__(self, config_file = 'genthemall.cfg', template_folder = None,
-        gtl_template_holder = None):
+    """GTLGenerator"""
+    def __init__(self, config_file = 'genthemall.cfg', template_folder = None, gtl_template_holder = None, out_dir = 'out'):
+        self.outDir = out_dir
         self.config = None
         if gtl_template_holder and isinstance(gtl_template_holder \
             , GTLTemplateHolder):
             self.gtlTemplateHolder = gtl_template_holder
         else:
             self.gtlTemplateHolder = GTLTemplateHolder(template_folder)
+        self.config_file = config_file
         self._load_config(config_file)
+
+    def generate(self, template_names):
+        """Generate with sepecify template names."""
+        if self.config is None or self.gtlTemplateHolder is None:
+            log.error('Config load fail or Template load fail.')
+            return False
+        if template_names is None or len(template_names) < 1:
+            log.error('Param template_names cannot be null or empty')
+            return False
+        if isinstance(template_names, str):
+            template_names = [t.strip() for t in template_names.split(',')]
+        if not isinstance(template_names, list):
+            log.error('Param template_names connot convert to list')
+            return False
+        succ_counter = 0
+        fail_counter = 0
+        for t_name in template_names:
+            t = self.gtlTemplateHolder.find_template_by_name(t_name)
+            if t is None:
+                log.warn('Template name[%s] not found' % t_name)
+                fail_counter += 1
+                continue
+            if self._generate_by_template(t):
+                succ_counter += 1
+            else:
+                fail_counter += 1
+        return (succ_counter, fail_counter)
         
+    def _generate_by_template(self, template):
+        succ_counter = 0
+        fail_counter = 0
+        for module in self.config.modules:
+            content = MakoTemplate(template.content).render( \
+                config=self.config, module=module)
+            if self._generate_to_file(content, template, module):
+                succ_counter += 1
+            else: 
+                fail_counter += 1
+        return (succ_counter, fail_counter)
+
+    def _generate_to_file(self, content, template, module):
+        """Generate content and write to file."""
+        dest = MakoTemplate(template.config.dest).render( \
+            config=self.config, module=module)
+        dest = os.path.join(self.outDir, dest)
+        destDir = os.path.dirname(dest)
+        if not os.path.exists(destDir):
+            os.makedirs(destDir)
+        file = open(dest, 'w')
+        file.write(content)
+        file.close()
+        return True
+
 
     def _load_config(self, config_file):
         """Load config file convert to python object."""
         log.debug('Loading config file[%s]...', config_file)
         self.config = Config(config_file)
+        if not hasattr(self.config, 'modules'):
+            log.warn('Module in config file must have modules property.')   
+        else:
+            self._init_modules_config(self.config.modules)
+
+    def _init_modules_config(self, modules):
+        """Init module properties, Add property pascalName, camelName, tableName to module."""
+        for m in modules:
+            msg_tmpl = 'Module in config file must have [%%s] property.'
+            msg_list = []
+            if not hasattr(m, 'name'):
+                msg_list.append(msg_tmpl % 'name')
+            if not hasattr(m, 'properties'):
+                msg_list.append(msg_tmpl % 'properties')
+            if msg_list:
+                log.warn('\n'.join(msg_list))
+            else:
+                if not hasattr(m, 'pascalName'):
+                    m.pascalName = _to_pascal_name(m.name)
+                if not hasattr(m, 'camelName'):
+                    m.camelName = _to_camel_name(m.name)
+                if not hasattr(m, 'tableName'):
+                    m.tableName = _to_upper_name(m.name)
+            
+            if not self._check_module_properties(m.properties):
+                continue
+
+
+    def _check_module_properties(self, properties):
+        for p in properties:
+            msg_tmpl = 'Must have [%%s] property in properties.'
+            if not hasattr(p, 'name'):
+                log.warn(msg_tmpl % 'name')
+                return False
+            else:
+                if not hasattr(p, 'pascalName'):
+                    p.pascalName = _to_pascal_name(p.name)
+                if not hasattr(p, 'camelName'):
+                    p.camelName = _to_camel_name(p.name)
+                if not hasattr(p, 'columnName'):
+                    p.columnName = _to_upper_name(p.name)
+
+            if not hasattr(p, 'type'):
+                log.warn(msg_tmpl % 'type')
+                return False
+                
+            else:
+                p.javaType = java_type_map[p.type]
+                p.javaShortType = java_short_type_map[p.type]
+                p.databaseType = database_type_map[p.type]
+                if not hasattr(p, 'min'):
+                    p.min = default_min_map[p.type]
+                if not hasattr(p, 'max'):
+                    p.max = default_max_map[p.type]
+            
+        return True
+
+    def print_config(self, obj = None, i = 0):
+        """Print config string in format."""
+        if obj == None:
+            self.print_config(self.config)
+            return
+        for k in obj:
+            if isinstance(k, str) and isinstance(obj[k], str):
+                print u'%s%-20s: %s' % (u' '*i, k, obj[k].decode('utf-8'))
+            elif isinstance(k, str) and isinstance(obj[k], \
+                                                  Sequence):
+                self.print_config(obj[k], i+1)
+                print ''
+            elif isinstance(k, Mapping):
+                self.print_config(k, i+1)
+                print ''
+
+### class GTLGenerator
+
+
+def _to_camel_name(name):
+    """
+    Convert name to camel name.
+    example:
+    SysUser will convert to sysUser
+    SysRole will convert to sysRole
+    """
+    if name is not None and len(name) > 1:
+        return name[0].lower() + name[1:]
+    return name
+
+def _to_pascal_name(name):
+    """
+    Convert name to pascal name.
+    example:
+    sysUser will convert to SysUser
+    sysRole will convert to SysRole
+    """
+    if name is not None and len(name) > 1:
+        return name[0].upper() + name[1:]
+    return name
+
+def _to_upper_name(name):
+    """
+    Convert name to uppercase name. 
+    example: 
+    sysUser will convert to SYS_USER
+    sysRole will convert to SYS_ROLE
+    """
+    if name is not None and len(name) > 1:
+        u_name = ''
+        for s in name:
+            if s == s.upper():
+                u_name += '_' + s
+            else:
+                u_name += s.upper()
+        return u_name
+    return name
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,6 +4,8 @@ from nose.tools import eq_, ok_
 
 def test_gtl_generator():
     g = GTLGenerator('tests/genthemall.cfg')
+    g.print_config()
+    ok_(g.generate('create_database,java_base_model'))
     ok_(g.config is not None)
 
 def test_find_templates():
